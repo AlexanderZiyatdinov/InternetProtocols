b5544861d4189b913e80e700ee9f27b5c3109625
Alexander Ziyatdinov <50361221+alextttzzz@users.noreply.github.com>
2020-06-27 18:52:43 +0500

    Add files via upload

--- portscan.py
+++ portscan.py
@@ -6,131 +6,104 @@ import queue
 import random
 import os
 
-PROTOCOLS = {'NTP': b'NTP', 'DNS': b'DNS', 'SMTP': b'SMTP', 'POP3': b'POP3',
-             'IMAP': b'IMAP', 'HTTP': b'HTTP'}
 time = random.randint(2 ** 16, 2 ** 64 - 1).to_bytes(8, 'big')
 UDP_CONST = b'\x13' + b'\0' * 39 + time
 
-TCP = 't'
-UDP = 'u'
-
 
 class Scanner:
-    def __init__(self, host: str, start_port: int = 1, end_port: int = 65535,
-                 tcp: bool = True,
-                 udp: bool = True, timeout: int = 0.5,
-                 workers: int = os.cpu_count() - 1):
+    def __init__(self, host: str, ports, tcp: bool = True, udp: bool = True,
+                 timeout: int = 1, workers: int = os.cpu_count() - 1):
         self.host = host
-        self.ports = make_queue(start_port, end_port, tcp, udp)
-        socket.setdefaulttimeout(timeout)
-
+        self.ports = _make_queue(ports, tcp, udp)
         self.to_print = queue.Queue()
-        self.isWorking = True
-
-        self.threads = [threading.Thread(target=self.run) for _ in
-                        range(workers)]
+        self.threads = [threading.Thread(target=self.run, daemon=True)
+                        for _ in range(workers)]
+        socket.setdefaulttimeout(timeout)
 
     def start(self):
         for t in self.threads:
-            t.setDaemon(True)
             t.start()
-        while not self.ports.empty() and self.isWorking:
+        while self.ports.qsize() > 0:
             try:
                 print(self.to_print.get(block=False))
             except queue.Empty:
                 pass
-
         for t in self.threads:
             t.join()
-
-        while not self.to_print.empty():
+        while not self.to_print.qsize() == 0:
             print(self.to_print.get())
 
-    def stop(self):
-        self.isWorking = False
-        for t in self.threads:
-            t.join()
-
     def run(self):
-        while self.isWorking:
-            try:
-                transport_protocol, port = self.ports.get(block=False)
-            except queue.Empty:
-                break
-            else:
-                if transport_protocol == TCP:
-                    self.get_tcp(port)
-                if transport_protocol == UDP:
-                    self.get_udp(port)
+        try:
+            while True:
+                conn_type, port = self.ports.get(block=False)
+                if conn_type == 'tcp':
+                    self._get_tcp(port)
+                elif conn_type == 'udp':
+                    self._get_udp(port)
+        except queue.Empty:
+            return
 
-    def get_tcp(self, port):
-        sock = socket.socket()
+    def _get_tcp(self, port):
+        serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        connect = serv_sock.connect_ex((self.host, port))
         data_end = b'\r\n\r\n'
-        try:
-            sock.connect((self.host, port))
-        except socket.error:
-            pass
-        except ConnectionResetError:
-            pass
-        except socket.timeout:
-            pass
-        else:
-            sock.send(b'a' * 250 + data_end)
-            data = sock.recv(1024)
-            self.to_print.put(f'TCP {port}{define_protocol(data)}')
-        finally:
-            sock.close()
+        if not connect:
+            try:
+                sock = socket.socket()
+                sock.connect((self.host, port))
+                sock.send(b'a' * 250 + data_end)
+                data = sock.recv(1024)
+                self.to_print.put(f'TCP {port} {define_protocol(data)}')
+                sock.close()
+            except Exception:
+                self.to_print.put(f'TCP {port}')
 
-    def get_udp(self, port):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        try:
-            sock.sendto(UDP_CONST, (self.host, port))
-            data, host = sock.recvfrom(1024)
-        except ConnectionResetError:
-            pass
-        except socket.timeout:
-            self.to_print.put('UDP {port}'.format(port=port))
-        else:
-            self.to_print.put('UDP {port}{define_protocol(data)}'
-                              .format(port=port, data=data))
-        finally:
-            sock.close()
+        serv_sock.close()
+
+    def _get_udp(self, port):
+        serv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        connect = serv_sock.connect_ex((self.host, port))
+        if not connect:
+            try:
+                sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                sock.sendto(UDP_CONST, (self.host, port))
+                data, host = sock.recvfrom(1024)
+                self.to_print.put(f'UDP {port} {define_protocol(data)}')
+                sock.close()
+            except ConnectionRefusedError:
+                pass
+            except socket.timeout:
+                pass
+            # self.to_print.put(f'UDP {port}')
+        serv_sock.close()
 
 
-def make_queue(start_port, end_port, tcp, udp):
+def _make_queue(port_pool, tcp, udp):
     q = queue.Queue()
-    for i in range(start_port, end_port + 1):
-        if tcp:
-            q.put((TCP, i))
-        if udp:
-            q.put((UDP, i))
+    [q.put(i) for i in
+     [('tcp', x) for x in port_pool if tcp] + [('udp', x) for x in port_pool if
+                                               udp]]
     return q
 
 
 def define_protocol(data):
     data_length = len(data)
-    if data_length > 4 and data[:4] == PROTOCOLS['HTTP']:
-        return ' HTTP'
-
-    if PROTOCOLS['SMTP'] in data:
-        return ' SMTP'
-
-    if PROTOCOLS['POP3'] in data:
-        return ' POP3'
-
-    if PROTOCOLS['IMAP'] in data:
-        return ' IMAP'
-
-    if data_length > 11 and data[:2] == UDP_CONST[:2] and (data[3] & 1) == 1:
-        return ' DNS'
-
+    if data_length > 4 and data[:4] == b'HTTP':
+        return 'HTTP'
+    if b'SMTP' in data:
+        return 'SMTP'
+    if b'POP3' in data:
+        return 'POP3'
+    if b'IMAP' in data:
+        return 'IMAP'
+    if (data_length > 11 and data[:2] == UDP_CONST[:2] and
+            data[3] & 1 == 1):
+        return 'DNS'
     if data_length > 39:
-        mode = 7 & data[0]
-        version = (data[0] >> 3) & 7
-
-        if mode == 4 and version == 2 and time == data[24:32]:
-            return ' NTP'
-
+        if (7 & data[0] == 4 and
+                (data[0] >> 3) & 7 == 2 and time == data[24:32]):
+            return 'NTP'
     return ''
 
 
@@ -160,12 +133,12 @@ def arg_parse(args):
 def main(args):
     args = arg_parse(args[1:])
 
-    scanner = Scanner(args.host, args.ports[0], args.ports[1],
+    scanner = Scanner(args.host, range(args.ports[0], args.ports[1] + 1),
                       args.tcp, args.udp, workers=os.cpu_count() - 1)
     try:
         scanner.start()
     except KeyboardInterrupt:
-        scanner.stop()
+        sys.exit(-1)
 
 
 if __name__ == "__main__":
